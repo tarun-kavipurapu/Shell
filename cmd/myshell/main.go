b8caebb01fc6e3379ec3f914d335f321e3894c41
Tarun <tarunsharmakavipurapu@gmail.com>
2024-12-24 09:30:46 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,9 +11,9 @@ var _ = fmt.Fprint
 
 func main() {
 	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 	// Wait for user input
 	for {
+		fmt.Fprint(os.Stdout, "$ ")
 		ansString, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Println(err)
